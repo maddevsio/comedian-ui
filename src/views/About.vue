e5c671173376d32d2f734b10309a4dffee1213f7
ZhanylTurtemirova <turtemirova.zhanyl@gmailcom>
2019-03-19 18:44:19 +0600

    Closes #5 add side nav

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <SideNav/>
   </div>
 </template>
+
+<script>
+import SideNav from '@/components/navigation/SideNav.vue'
+
+export default {
+  name: 'home',
+  components: {
+    SideNav
+  }
+}
+</script>
