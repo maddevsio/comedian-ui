230636821e33f9adc57eee122f2fdfa4ab2439b4
ZhanylTurtemirova <turtemirova.zhanyl@gmailcom>
2019-03-22 11:23:10 +0600

    Closes #11 add field options to  table

--- src/components/comedian/Channels.vue
+++ src/components/comedian/Channels.vue
@@ -1,17 +1,18 @@
 <template>
 <div>
   <v-data-table
-    :headers="headers"
+  :headers="headers"
     :items="channels"
     class="elevation-1"
   >
    <template v-slot:items="props">
       <td>{{ props.item.name }}</td>
-      <td class="text-xs-right">{{ props.item.id }}</td>
-      <td class="text-xs-right">{{ props.item.team_id }}</td>
-      <td class="text-xs-right">{{ props.item.channel_name }}</td>
-      <td class="text-xs-right">{{ props.item.channel_id }}</td>
-      <td class="text-xs-right">{{ props.item.channel }}</td>
+      <td class="text-xs-left">{{ props.item.id }}</td>
+      <td class="text-xs-left">{{ props.item.team_id }}</td>
+      <td class="text-xs-left">{{ props.item.channel_name }}</td>
+      <td class="text-xs-left">{{ props.item.channel_id }}</td>
+      <td class="text-xs-left">{{ props.item.channel_standup_time }}</td>
+      <td class="text-xs-left"><span> Edit /</span><span> Delete</span></td>
     </template>   
   </v-data-table>
   <p>{{ errors }}</p>
@@ -25,17 +26,16 @@ import axios from  'axios'
       return {
         errors:[],
          headers: [
-          {
-            text: 'Channels',
-            align: 'left',
-            sortable: false,
-            value: 'name'
+          { 
+            text:'Channels',
+            align: 'center'
           },
           { text: 'ID', value: 'id' },
           { text: 'Team_id', value: 'team_id' },
           { text: 'Channel_name', value: 'channel_name' },
           { text: 'Channel_id', value: 'channel_id' },
-          { text: 'Channel', value: 'channel' }
+          { text: 'Channel_standup_time', value: 'channel_standup_time' },
+          { text: 'Options', value: '' }
         ],
         channels:[],
       }
@@ -44,9 +44,9 @@ import axios from  'axios'
      axios.get('https://staging.comedian.maddevs.co/v1/channels').then((response) => {
       this.channels = response.data
     })
-    .catch((e) => {
-      this.errors.push(e);
-    })
+      .catch((e) => {
+        this.errors.push(e);
+      })
     }
   }
 </script>
