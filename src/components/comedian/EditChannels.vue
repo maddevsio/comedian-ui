b64e6c6319af765d2340a6f9fdf0a8b56e94c7bb
ZhanylTurtemirova <turtemirova.zhanyl@gmailcom>
2019-03-28 12:49:33 +0600

    Fixes #33: fix channels card and axios request

--- src/components/comedian/EditChannels.vue
+++ src/components/comedian/EditChannels.vue
@@ -1,58 +1,51 @@
 <template lang="html">
-<div> 
-  <v-form  method="post">
-    <v-container>
-       <v-layout row justify-center>
-        <v-flex
-          xs12
-          md4
-        >
-          <v-text-field
-            v-model="channel.channel_standup_time"
-            label="Channel Standup Time"
-            type="number"
-            required
-          />
-        </v-flex>       
-      </v-layout>
-    </v-container>
-    <v-btn @click='Save'
-    >
-     Save
-    </v-btn>
-  </v-form>
-  </div>
+  <v-card class="mt-12 mx-auto" max-width="1200"> 
+    <v-form  method="post">
+      <v-container>
+        <v-layout row justify-center>
+          <v-flex
+          >
+            <v-text-field
+              v-model="channel.channel_standup_time"
+              label="Channel Standup Time"
+              type="number"
+              required
+            />
+          </v-flex>       
+        </v-layout>
+      </v-container>
+      <v-btn @click='Save'>
+      Save
+      </v-btn>
+    </v-form>
+  </v-card>
 </template>
 
 <script>
-import axios from  'axios'
-import transform from '../../helpers/transform'
+import axios from "axios";
+import transform from "../../helpers/transform";
+import { mapState } from "vuex";
+
 export default {
-  data() {
-    return{
-      channel: []
+  computed: mapState({
+    channel: state => state.channels.channels
+  }),
+  methods: {
+    async Save() {
+      const url = `channels/${this.$route.params.id}`;
+      const transformedValues = transform(this.channel, {
+        channel_standup_time: "int"
+      });
+
+      await this.$store.dispatch("UPDATE_CHANNEL", {
+        url,
+        data: transformedValues
+      });
     }
   },
-  methods: {
-     Save() {
-       const transformedValues = transform(this.channel, {
-         channel_standup_time: 'int'
-       }) 
-      axios.patch(`https://staging.comedian.maddevs.co/v1/channels/${this.$route.params.id}`, {
-        ...transformedValues,
-      }).then(()=> {
-        alert("Изменения сохранены")
-      });      
-    }   
-  },
-   created() {
-    axios.get(`https://staging.comedian.maddevs.co/v1/channels/${this.$route.params.id}`).then((response) => {
-      this.channel = response.data
-      console.log(this.channel)
-    })
-    .catch((e) => {
-      console.log(e)
-    })
+  beforeCreate() {
+    const url = `channels/${this.$route.params.id}`;
+    this.$store.dispatch("GET_CHANNELS", url);
   }
-}
+};
 </script>
